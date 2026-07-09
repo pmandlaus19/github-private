bea81cf34bbef9a9fca5ad7c009798bd3dcbd28b
pmandlaus_19 <pmandlaus@adobe.com>
2026-07-09 13:12:24 +0530

    uploading sidenav to: https://api.github.com/repos/pmandlaus19/github-private/contents/blocks/sidenav/sidenav.js

--- blocks/sidenav/sidenav.js
+++ blocks/sidenav/sidenav.js
@@ -1,6 +1,6 @@
 import { migrateTree } from "../utils.js";
 const treeData = [{"displayName":"test_topic","url":"contents/test-topic"}]
-const mapTitle = "testMap"
+const mapTitle = "Test book map"
 const isDesktop = window.matchMedia("(min-width: 900px)");
 
 function expandHeirarchy(element, root) {
