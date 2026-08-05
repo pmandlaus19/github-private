84fc7ad636ff49de442e1ba9dece95971dd66860
pmandlaus_19 <pmandlaus@adobe.com>
2026-08-05 14:33:09 +0530

    uploading sidenav to: https://api.github.com/repos/pmandlaus19/github-private/contents/blocks/sidenav/sidenav.js

--- blocks/sidenav/sidenav.js
+++ blocks/sidenav/sidenav.js
@@ -1,6 +1,6 @@
 import { migrateTree } from "../utils.js";
-const treeData = [{"displayName":"Vehicle Structure","url":"contents/vehicle-structure"},{"displayName":"Overview","url":"contents/overview"},{"displayName":"Legal","url":"contents/legal"},{"displayName":"Landing System","url":"contents/hr/landing-system"},{"displayName":"Escape System","url":"contents/hr/escape-system"},{"displayName":"Engine Limitations","url":"contents/engine-limitations"},{"displayName":"Crew and passenger system","url":"contents/crew-and-passenger-system"}]
-const mapTitle = "gitmap"
+const treeData = [{"displayName":"Media-Topic","url":"contents/media-topic"},{"displayName":"Image-content-testing-","url":"contents/image-content-testing-"},{"displayName":"indirect-Topic","url":"contents/indirect-topic"}]
+const mapTitle = "Multimedia Report"
 const isDesktop = window.matchMedia("(min-width: 900px)");
 
 function expandHeirarchy(element, root) {
